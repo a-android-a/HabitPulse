174ef6962b8468d8e71b2700eb1072f5144f2f7f
a_android_a <androidnal@yandex.ru>
2025-10-06 17:25:04 +0300

    added a table and processing of the add button

--- HabitPulse/main.cpp
+++ HabitPulse/main.cpp
@@ -6,7 +6,10 @@
 #include <QMessageBox>
 #include <QLineEdit>
 #include <QVBoxLayout>
+#include <QTableWidget>
+#include <QLabel>
 #include "LoadStyle.h"
+
 int main (int argc, char** argv){
     QApplication app(argc, argv);
 
@@ -18,12 +21,28 @@ int main (int argc, char** argv){
     QPushButton *buttonAdd = new QPushButton();
     QVBoxLayout *VBox = new QVBoxLayout();
     QLineEdit *inputField = new QLineEdit();
-
+    QTableWidget *tableOfHabits = new QTableWidget(0, 1);
+    QLabel *label = new QLabel("basic habits");
     inputField->setPlaceholderText("habit");
     buttonAdd->setText("Add");
+    tableOfHabits->setHorizontalHeaderItem(1, new QTableWidgetItem("Name of the habit"));
+    tableOfHabits->resizeColumnsToContents();
 
     layout->addWidget(inputField);
     layout->addWidget(buttonAdd);
+    layout->addWidget(label);
+    layout->addWidget(tableOfHabits);
+
+
+    QObject::connect(buttonAdd, &QPushButton::clicked, [tableOfHabits,inputField]() {
+        tableOfHabits->setRowCount(tableOfHabits->rowCount() + 1  );
+        QTableWidgetItem *textItem = new QTableWidgetItem(inputField->text());
+        tableOfHabits->setItem(tableOfHabits->rowCount()-1, 0, textItem);
+        tableOfHabits->resizeColumnsToContents();
+        inputField->clear();
+    });
+
+
     window.setLayout(layout);
     window.show();
     return app. exec();
