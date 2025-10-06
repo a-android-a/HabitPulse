19eb78947511322b2550920b877e592c63d42ad4
a_android_a <androidnal@yandex.ru>
2025-10-06 15:57:45 +0300

    added LoadStyle function

---
+++ HabitPulse/LoadStyle.h
@@ -0,0 +1,17 @@
+#ifndef LOADSTYLE_H
+#define LOADSTYLE_H
+#include <QFile>
+#include <QString>
+
+bool LoadStyle(const QString &pathStyle){
+    QFile fileStyle(pathStyle);
+    if(fileStyle.open(QFile::ReadOnly)){
+        QString strCSS = QLatin1String(fileStyle.readAll());
+        qApp->setStyleSheet (strCSS);
+        return true;
+    }else  {
+        return false;
+    }
+}
+
+#endif // LOADSTYLE_H
--- HabitPulse/main.cpp
+++ HabitPulse/main.cpp
@@ -3,30 +3,28 @@
 #include<QPushButton>
 #include<QPalette>
 #include <QFile>
-
+#include"LoadStyle.h"
 int main (int argc, char** argv){
     QApplication app(argc, argv);
 
     QWidget wgt;
 
     wgt.setBackgroundRole(QPalette::ColorRole::Base);
+    LoadStyle("style.qss");
     QPushButton* pcmdA = new QPushButton ("Button1");
     QPushButton* pcmdB = new QPushButton ("Button2");
     QPushButton* pcmdC = new QPushButton ("Button3");
     //Layout setup
     QVBoxLayout* pvbxLayout = new QVBoxLayout;
     pvbxLayout->addWidget (pcmdA);
-    pvbxLayout->addWidget (pcmdB) ;
+    pvbxLayout->addWidget (pcmdB);
     pvbxLayout->addWidget (pcmdC);
     wgt.setLayout (pvbxLayout);
     QObject::connect(pcmdA, &QPushButton::clicked, &app, &QApplication::beep);
     QObject::connect(pcmdB, &QPushButton::clicked, &app, &QApplication::beep);
     QObject::connect(pcmdC, &QPushButton::clicked, &app, &QApplication::beep);
-    QFile file("style.qss");
-    file.open (QFile::ReadOnly);
-    QString strCSS = QLatin1String(file.readAll());
-    qDebug() << strCSS;
-    qApp->setStyleSheet (strCSS);
+
+
     wgt.show();
     return app. exec();
 }
