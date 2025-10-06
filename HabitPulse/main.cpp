f99e7aff17bc68e3aa9100099cfa3f53933d83f5
a_android_a <androidnal@yandex.ru>
2025-10-06 15:01:25 +0300

    testing styles

--- HabitPulse/main.cpp
+++ HabitPulse/main.cpp
@@ -1,11 +1,33 @@
-    #include "mainwindow.h"
+#include <QtWidgets>
+#include<QVBoxLayout>
+#include<QPushButton>
+#include<QPalette>
+#include <QFile>
 
-#include <QApplication>
+int main (int argc, char** argv){
+    QApplication app(argc, argv);
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QWidget wgt;
+
+    wgt.setBackgroundRole(QPalette::ColorRole::Base);
+    QPushButton* pcmdA = new QPushButton ("Button1");
+    QPushButton* pcmdB = new QPushButton ("Button2");
+    QPushButton* pcmdC = new QPushButton ("Button3");
+    //Layout setup
+    QVBoxLayout* pvbxLayout = new QVBoxLayout;
+    pvbxLayout->addWidget (pcmdA);
+    pvbxLayout->addWidget (pcmdB) ;
+    pvbxLayout->addWidget (pcmdC);
+    wgt.setLayout (pvbxLayout);
+    QObject::connect(pcmdA, &QPushButton::clicked, &app, &QApplication::beep);
+    QObject::connect(pcmdB, &QPushButton::clicked, &app, &QApplication::beep);
+    QObject::connect(pcmdC, &QPushButton::clicked, &app, &QApplication::beep);
+    QFile file("style.qss");
+    file.open (QFile::ReadOnly);
+    QString strCSS = QLatin1String(file.readAll());
+    qDebug() << strCSS;
+    qApp->setStyleSheet (strCSS);
+    wgt.show();
+    return app. exec();
 }
+
