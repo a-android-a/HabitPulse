f79c40b718e06d6f6a4f77a89746a476c3875f1a
a_android_a <androidnal@yandex.ru>
2025-10-08 15:41:15 +0300

    added class: MainWidget

---
+++ HabitPulse/MainWidget.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<QWidget>
+
+class QPushButton;
+class QVBoxLayout;
+class QHBoxLayout;
+class QLineEdit;
+class QTableWidget;
+class QLabel;
+
+class MainWidget: public QWidget {
+    Q_OBJECT
+private:
+    QPushButton *buttonAdd;
+    QPushButton *buttonDel;
+    QHBoxLayout *HBox;
+    QLineEdit *inputField;
+    QTableWidget *tableOfHabits;
+    QLabel *label;
+public:
+    MainWidget(QWidget* pwgt  = 0 );
+
+private slots:
+    void AddButtonClicked();
+    void DeleteButtonClicked();
+};
+
